cdddc844af16b0c8581df4fde5a56b3e9de29652
FerkelBorsti <ferkelborstikcknuppeldick@gmail.com>
2026-08-27 16:07:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    und nur bei fehlendem Empfang aus dem Zwischenspeicher. Vorher galt umgekehrt
    "Zwischenspeicher zuerst" – dadurch startete die App auch mit Netz noch tagelang
    in der alten Fassung. Offline funktioniert unverändert, nur eben als Rückfall. */
-const VERSION = 'kc-v1.7.2';
+const VERSION = 'kc-v1.8.0';
 const HUELLE = [
   './',
   './index.html',
