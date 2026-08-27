0c907a765969a2e5226711b5470be6d19c74ce21
FerkelBorsti <ferkelborstikcknuppeldick@gmail.com>
2026-08-27 22:26:02 +0200

    Update kc-push-konfig.js

--- kc-push-konfig.js
+++ kc-push-konfig.js
@@ -13,10 +13,10 @@
    läuft dann unverändert, der Schalter taucht in den Einstellungen nicht auf. */
 
 self.KC_PUSH = {
-  apiKey:            '',
-  authDomain:        '',
-  projectId:         '',
-  messagingSenderId: '',
-  appId:             '',
-  vapidKey:          ''
+  apiKey:            'AIzaSyAiHHf-j9rSYL36DEmeRAtue9Xl4tWOVgY',
+  authDomain:        'kc-knueppeldick.firebaseapp.com',
+  projectId:         'kc-knueppeldick',
+  messagingSenderId: '553048498685',
+  appId:             '1:553048498685:web:c8b3c26046e30a837bb994',
+  vapidKey:          'BJeYO9No8FHnOT7NF1NlcXWMYPkrpMep8XKhT1C_E8UQV99TebBN0HYyVD4zdGE5wNmCjfSlBp6EUm0stw5bAqg'
 };
